1ceef41b506f5af25975e4502cc619cc55570190
Maryum Fiaz <maryumfiazbaig333@gmail.com>
2026-06-01 00:03:46 +0500

    invalid id, validation err to follow db rules

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -1,4 +1,4 @@
-
+import ErrorHandler from "../utils/errorHandler.js";
 
 export default (err, req, res, next) => {
 
@@ -6,6 +6,18 @@ export default (err, req, res, next) => {
         statusCode : err?.statusCode || 500,
         message : err?.message || "Internal Server Error",
     }
+
+    // Handle Invalid Mongoose ID Error
+  if (err.name === "CastError") {
+    const message = `Resource not found. Invalid: ${err?.path}`;
+    error = new ErrorHandler(message, 404);
+  }
+
+  // Handle Validation Error
+  if (err.name === "ValidationError") {
+    const message = Object.values(err.errors).map((value) => value.message);
+    error = new ErrorHandler(message, 400);
+  }
 
 
 
