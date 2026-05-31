8a0ede7bd9942558c1a75c5bc5430db7a28ca09a
Maryum Fiaz <maryumfiazbaig333@gmail.com>
2026-05-31 22:49:50 +0500

    dev vs prod errors msg display

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -8,8 +8,22 @@ export default (err, req, res, next) => {
     }
 
 
-    res.status(error.statusCode).json({
-        message: error.message,
-    })
+
+    // Development error to display
+    if(process.env.NODE_ENV === 'DEVELOPMENT') {
+        res.status(error.statusCode).json({
+            message: error.message,
+            error: err,
+            stack: err?.stack,
+        })
+    }
+
+    // Production error msg to display
+    if(process.env.NODE_ENV === 'PRODUCTION') {
+
+        res.status(error.statusCode).json({
+            message: error.message,
+        })
+    }
 
 }
