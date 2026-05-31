7bf7622c0996de0815d9cee8dedc0bda329495fc
Maryum Fiaz <maryumfiazbaig333@gmail.com>
2026-05-31 23:25:49 +0500

    unhandled promise rej, uncaught exception

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,13 @@ const app = express()
 import { connectDatabase } from './config/dbConnect.js'
 import errorMiddleware from './middleware/error.js'
 
+// Handle uncaught exception
+process.on('uncaughtException', (err) => {
+    console.log(`ERROR: ${err}`);
+    console.log('Shutting down server due to uncaught exception');
+    process.exit(1);
+})
+
 dotenv.config({path: 'backend/config/config.env'})
 
 // Database connection
@@ -19,6 +26,15 @@ app.use('/api/v1', productRoutes)
 app.use(errorMiddleware)
 
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
   console.log(`App listening on port ${process.env.PORT} in ${process.env.NODE_ENV} mode`)
+})
+
+// Handle Unhandled promise rejection (async error)
+process.on('unhandledRejection', (err) => {
+    console.log(`ERROR: ${err}`);
+    console.log('Shutting down server due to unhandled Promise Rejection');
+    server.close(()=>{
+        process.exit(1);
+    });     // server shut down
 })
