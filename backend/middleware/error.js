b48b7d626eee81d8b667b94a2a14712dd6602937
Maryum Fiaz <maryumfiazbaig333@gmail.com>
2026-06-11 17:32:38 +0500

    jwt expire error

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -1,13 +1,12 @@
 import ErrorHandler from "../utils/errorHandler.js";
 
 export default (err, req, res, next) => {
+  let error = {
+    statusCode: err?.statusCode || 500,
+    message: err?.message || "Internal Server Error",
+  };
 
-    let error = {
-        statusCode : err?.statusCode || 500,
-        message : err?.message || "Internal Server Error",
-    }
-
-    // Handle Invalid Mongoose ID Error
+  // Handle Invalid Mongoose ID Error
   if (err.name === "CastError") {
     const message = `Resource not found. Invalid: ${err?.path}`;
     error = new ErrorHandler(message, 404);
@@ -31,23 +30,25 @@ export default (err, req, res, next) => {
     error = new ErrorHandler(message, 400);
   }
 
+  // Handle expired JWT Error
+  if (err.name === "TokenExpiredError") {
+    const message = `JSON Web Token is expired. Try Again!!!`;
+    error = new ErrorHandler(message, 400);
+  }
 
+  // Development error to display
+  if (process.env.NODE_ENV === "DEVELOPMENT") {
+    res.status(error.statusCode).json({
+      message: error.message,
+      error: err,
+      stack: err?.stack,
+    });
+  }
 
-    // Development error to display
-    if(process.env.NODE_ENV === 'DEVELOPMENT') {
-        res.status(error.statusCode).json({
-            message: error.message,
-            error: err,
-            stack: err?.stack,
-        })
-    }
-
-    // Production error msg to display
-    if(process.env.NODE_ENV === 'PRODUCTION') {
-
-        res.status(error.statusCode).json({
-            message: error.message,
-        })
-    }
-
-}
+  // Production error msg to display
+  if (process.env.NODE_ENV === "PRODUCTION") {
+    res.status(error.statusCode).json({
+      message: error.message,
+    });
+  }
+};
