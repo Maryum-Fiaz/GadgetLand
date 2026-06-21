b7fed48bc6d82fd5904e6844e17c372c955bfa9f
Maryum Fiaz <maryumfiazbaig333@gmail.com>
2026-06-21 21:46:21 +0500

    Scroll to top , ui fix

--- frontend/src/RootLayout.jsx
+++ frontend/src/RootLayout.jsx
@@ -2,11 +2,13 @@ import { Footer, Header, Container } from './components'
 import { Outlet } from 'react-router';
 import { HelmetProvider } from 'react-helmet-async';
 import { Toaster } from "react-hot-toast";
+import { ScrollToTop } from './components/index.js';
 
 function RootLayout() {
   return (
     <HelmetProvider>
       <Toaster position='top-center' />
+      <ScrollToTop />
 
     <Header />
     <Container>
