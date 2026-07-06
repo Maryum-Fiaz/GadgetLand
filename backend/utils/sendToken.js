e81cc21e576ebaf15b5eb2278e4edae3d9b4367e
Maryum Fiaz <maryumfiazbaig333@gmail.com>
2026-07-07 01:32:30 +0500

    fix: token

--- backend/utils/sendToken.js
+++ backend/utils/sendToken.js
@@ -1,19 +1,19 @@
 // Create token and save in the cookie
 export default (user, statusCode, res) => {
+  // Create JWT Token
+  const token = user.getJwtToken(); //user.js in models defined getJwtToken function
 
-
-    // Create JWT Token
-    const token = user.getJwtToken()     //user.js in models defined getJwtToken function
-
-    // Options for cookie
-    const options = {
-        expires: new Date(
-            Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
-        ),
-        httpOnly: true,
-    };
-                        // cookie function ("cookie name", value, options)
-    res.status(statusCode).cookie("token", token, options).json({
-        token,
-    })
+  // Options for cookie
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000,
+    ),
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "PRODUCTION",
+    sameSite: "none",
+  };
+  // cookie function ("cookie name", value, options)
+  res.status(statusCode).cookie("token", token, options).json({
+    token,
+  });
 };
